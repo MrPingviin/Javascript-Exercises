2686857f5ebd274bc7cd4fba3fb0ba466a98e743
MrPingviin <36996680+MrPingviin@users.noreply.github.com>
2020-06-12 20:24:52 +0200

    Bugfix
    For my first exercise.

--- my_first_exercise.js
+++ my_first_exercise.js
@@ -1,11 +1,12 @@
 /* Hey, this is my first Javascript exercise c:
- Current version: v1.0
+ Current version: v1.1
 
- Known bugs: When you set your name as Bob, you still can see the alert in the 'else' section.
+ Fixed bugs: You no longer get the else's alert when you give Bob as name.
  */
 var name  = prompt("Set a name [Options: Bob, David / other]");
 if (name === "Bob") {
     alert("Hey, Bob.");
+    end;
 }
 if (name === "David") {
     alert("Hi, David.");
